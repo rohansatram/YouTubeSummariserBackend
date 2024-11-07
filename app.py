2c72c3dd8a730362152400df64b1064ae25c2379
RohanSatram <satram.rohan@gmail.com>
2024-11-08 04:40:54 +0530

    Creating a flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template, jsonify
+from main import main
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+    url = request.form['url'] 
+    result = main(url) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
